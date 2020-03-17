906c968472ecb04c765bd575dda6839e774099ca
Alexander Marquardt <alexander.marquardt@elastic.co>
2020-03-17 19:37:40 +0000

    Did some renaming

--- convert_csv_to_json.js
+++ convert_csv_to_json.js
@@ -4,12 +4,12 @@
         // This function takes an array containing the field names, and another that
         // contains field values, and returns a json dictionary that combines them.
         function convert_csv_to_dict(csv_headers_row, csv_values_row) {
-          var json_dict =  csv_values_row.reduce(function(result, field, index) {
+          var json_from_csv =  csv_values_row.reduce(function(result, field, index) {
             result[csv_headers_row[index]] = field;
             return result;
           }, {})
 
-          return json_dict;
+          return json_from_csv;
         }
 
 
@@ -23,14 +23,14 @@
           return function(msg) {
 
             var csv_values_row = msg.split(CSV_SPLIT_CHAR);
-            var json_dict = null;
+            var json_from_csv = null;
             if (!csv_headers_row) {
               csv_headers_row = csv_values_row;
             } else {
               // combine the csv_headers_row with the values to get a dict
-              json_dict = convert_csv_to_dict(csv_headers_row, csv_values_row)
+              json_from_csv = convert_csv_to_dict(csv_headers_row, csv_values_row)
             }
-            return json_dict;
+            return json_from_csv;
           }
 
         })();  
@@ -40,14 +40,14 @@
         // (eg. called once for each line in the log file)
         function process(event) {
             var msg = event.Get("message");
-            var json_dict = headers_fn(msg);
+            var json_from_csv = headers_fn(msg);
 
             // If the current event was triggered to process the header row,
-            // then the json_dict will be empty - it only returns a json dict
+            // then the json_from_csv will be empty - it only returns a json dict
             // for subsequent rows. Cancel the event so that nothing
             // is sent to the output.
-            if (!json_dict) {
+            if (!json_from_csv) {
               event.Cancel();
             }
-            event.Put("json_dict", json_dict);
+            event.Put("json_from_csv", json_from_csv);
         }
