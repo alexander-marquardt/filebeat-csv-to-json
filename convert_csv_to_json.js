9e202eb2ef6910a3506a2a9cd0079f182eda7e90
Alexander Marquardt <alexander.marquardt@elastic.co>
2020-03-17 20:45:35 +0000

    Updated to make use of the built-in CSV processor, which handles non-standard CSV formats better than a simple split string

--- convert_csv_to_json.js
+++ convert_csv_to_json.js
@@ -1,5 +1,3 @@
-        var CSV_SPLIT_CHAR = ",";
-
 
         // This function takes an array containing the field names, and another that
         // contains field values, and returns a json dictionary that combines them.
@@ -16,19 +14,20 @@
         // Define the Javascript function that will be used to combine the 
         // header row with subsequent rows in the CSV file
         var headers_fn = (function() {
-
-          // Use a javascript closure to keep the header line (csv_headers_row) around 
-          // so that we can use the header values for all subsequent CSV entries. 
           var csv_headers_row = null; 
-          return function(msg) {
 
-            var csv_values_row = msg.split(CSV_SPLIT_CHAR);
+          // Use a javascript closure to store the header line (csv_headers_row), 
+          // so that we can use the header values for all subsequent CSV entries. 
+          return function(csv_arr) {
+
             var json_from_csv = null;
+
             if (!csv_headers_row) {
-              csv_headers_row = csv_values_row;
+              // if this is the first row, store the headers
+              csv_headers_row = csv_arr;
             } else {
               // combine the csv_headers_row with the values to get a dict
-              json_from_csv = convert_csv_to_dict(csv_headers_row, csv_values_row)
+              json_from_csv = convert_csv_to_dict(csv_headers_row, csv_arr)
             }
             return json_from_csv;
           }
@@ -39,8 +38,8 @@
         // This function is called for each "event" 
         // (eg. called once for each line in the log file)
         function process(event) {
-            var msg = event.Get("message");
-            var json_from_csv = headers_fn(msg);
+            var csv_arr = event.Get("decoded_csv_arr");
+            var json_from_csv = headers_fn(csv_arr);
 
             // If the current event was triggered to process the header row,
             // then the json_from_csv will be empty - it only returns a json dict
