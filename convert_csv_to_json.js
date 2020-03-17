95eb60aee3e11f9c5cb19e41f7f5eeb2a7b423e6
Alexander Marquardt <alexander.marquardt@elastic.co>
2020-03-17 00:19:50 +0000

    Re-named variables

--- convert_csv_to_json.js
+++ convert_csv_to_json.js
@@ -4,9 +4,9 @@
         // This function takes an array containing the field names, and another that
         // contains field values, and returns a json dictionary that combines them.
 
-        function convert_csv_to_dict(headers, values) {
-          var result =  values.reduce(function(result, field, index) {
-            result[headers[index]] = field;
+        function convert_csv_to_dict(csv_headers_row, csv_values_row) {
+          var result =  csv_values_row.reduce(function(result, field, index) {
+            result[csv_headers_row[index]] = field;
             return result;
           }, {})
 
@@ -14,25 +14,27 @@
         }
 
 
-
-        // Use a javascript _closure_ to keep the header line around so that we can use 
-        // the header values for all subsequent CSV entries. 
-
+        // self-calling javascript function that will be used to combine the 
+        // header row with subsequent rows in the CSV file
         var headers_fn = (function() {
-          var headers_arr = null; 
+
+          // Use a javascript closure to keep the header line (csv_headers_row) around 
+          // so that we can use the header values for all subsequent CSV entries. 
+          var csv_headers_row = null; 
           return function(msg) {
 
-            var split_vals = msg.split(CSV_SPLIT_CHAR);
+            var csv_values_row = msg.split(CSV_SPLIT_CHAR);
             var json_dict = null;
-            if (!headers_arr) {
-              headers_arr = split_vals;
+            if (!csv_headers_row) {
+              csv_headers_row = csv_values_row;
             } else {
-              // combine the headers_arr with the values to get a dict
-              json_dict = convert_csv_to_dict(headers_arr, split_vals)
+              // combine the csv_headers_row with the values to get a dict
+              json_dict = convert_csv_to_dict(csv_headers_row, csv_values_row)
             }
             return json_dict;
           }
-        })();  // self-calling javascript function
+
+        })();  
 
         
 
